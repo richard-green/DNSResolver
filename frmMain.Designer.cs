bef38735bd67b449c5c25e15500981a5fc27a651
Richard Green <richard.green@15below.com>
2013-10-09 12:53:40 +0100

    DNS Resolver first commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace DnsResolver
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.dgDnsEntries = new System.Windows.Forms.DataGridView();
+            this.btnResolveAll = new System.Windows.Forms.Button();
+            this.btnClearList = new System.Windows.Forms.Button();
+            this.btnFlushDNS = new System.Windows.Forms.Button();
+            this.Hostname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IPAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tmrQueuePoll = new System.Windows.Forms.Timer(this.components);
+            this.btnPasteFromClipboard = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDnsEntries)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgDnsEntries
+            // 
+            this.dgDnsEntries.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgDnsEntries.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgDnsEntries.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Hostname,
+            this.IPAddress});
+            this.dgDnsEntries.Location = new System.Drawing.Point(12, 12);
+            this.dgDnsEntries.Name = "dgDnsEntries";
+            this.dgDnsEntries.Size = new System.Drawing.Size(669, 484);
+            this.dgDnsEntries.TabIndex = 0;
+            this.dgDnsEntries.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgDnsEntries_CellValueChanged);
+            // 
+            // btnResolveAll
+            // 
+            this.btnResolveAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnResolveAll.Location = new System.Drawing.Point(606, 502);
+            this.btnResolveAll.Name = "btnResolveAll";
+            this.btnResolveAll.Size = new System.Drawing.Size(75, 23);
+            this.btnResolveAll.TabIndex = 1;
+            this.btnResolveAll.Text = "Resolve All";
+            this.btnResolveAll.UseVisualStyleBackColor = true;
+            this.btnResolveAll.Click += new System.EventHandler(this.btnResolveAll_Click);
+            // 
+            // btnClearList
+            // 
+            this.btnClearList.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClearList.Location = new System.Drawing.Point(444, 502);
+            this.btnClearList.Name = "btnClearList";
+            this.btnClearList.Size = new System.Drawing.Size(75, 23);
+            this.btnClearList.TabIndex = 2;
+            this.btnClearList.Text = "Clear List";
+            this.btnClearList.UseVisualStyleBackColor = true;
+            this.btnClearList.Click += new System.EventHandler(this.btnClearList_Click);
+            // 
+            // btnFlushDNS
+            // 
+            this.btnFlushDNS.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFlushDNS.Location = new System.Drawing.Point(525, 502);
+            this.btnFlushDNS.Name = "btnFlushDNS";
+            this.btnFlushDNS.Size = new System.Drawing.Size(75, 23);
+            this.btnFlushDNS.TabIndex = 3;
+            this.btnFlushDNS.Text = "Flush DNS";
+            this.btnFlushDNS.UseVisualStyleBackColor = true;
+            this.btnFlushDNS.Click += new System.EventHandler(this.btnFlushDNS_Click);
+            // 
+            // Hostname
+            // 
+            this.Hostname.HeaderText = "Hostname";
+            this.Hostname.Name = "Hostname";
+            this.Hostname.Width = 400;
+            // 
+            // IPAddress
+            // 
+            this.IPAddress.HeaderText = "IP Address";
+            this.IPAddress.Name = "IPAddress";
+            this.IPAddress.ReadOnly = true;
+            this.IPAddress.Width = 200;
+            // 
+            // tmrQueuePoll
+            // 
+            this.tmrQueuePoll.Enabled = true;
+            this.tmrQueuePoll.Interval = 500;
+            this.tmrQueuePoll.Tick += new System.EventHandler(this.tmrQueuePoll_Tick);
+            // 
+            // btnPasteFromClipboard
+            // 
+            this.btnPasteFromClipboard.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPasteFromClipboard.Location = new System.Drawing.Point(309, 502);
+            this.btnPasteFromClipboard.Name = "btnPasteFromClipboard";
+            this.btnPasteFromClipboard.Size = new System.Drawing.Size(129, 23);
+            this.btnPasteFromClipboard.TabIndex = 4;
+            this.btnPasteFromClipboard.Text = "Paste from Clipboard";
+            this.btnPasteFromClipboard.UseVisualStyleBackColor = true;
+            this.btnPasteFromClipboard.Click += new System.EventHandler(this.btnPasteFromClipboard_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 537);
+            this.Controls.Add(this.btnPasteFromClipboard);
+            this.Controls.Add(this.btnFlushDNS);
+            this.Controls.Add(this.btnClearList);
+            this.Controls.Add(this.btnResolveAll);
+            this.Controls.Add(this.dgDnsEntries);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "DNS Resolver";
+            ((System.ComponentModel.ISupportInitialize)(this.dgDnsEntries)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgDnsEntries;
+        private System.Windows.Forms.Button btnResolveAll;
+        private System.Windows.Forms.Button btnClearList;
+        private System.Windows.Forms.Button btnFlushDNS;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hostname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IPAddress;
+        private System.Windows.Forms.Timer tmrQueuePoll;
+        private System.Windows.Forms.Button btnPasteFromClipboard;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DnsResolver
+{
+    public partial class frmMain : Form
+    {
+        #region Members
+
+        Queue<string> dnsRequestsToDo = new Queue<string>();
+
+        #endregion Members
+
+        #region Constructor
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Constructor
+
+        #region User Events
+
+        private void dgDnsEntries_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex == 0)
+            {
+                EnqueueResolve(dgDnsEntries.Rows[e.RowIndex].Cells[e.ColumnIndex].Value as string);
+            }
+        }
+
+        private void btnClearList_Click(object sender, EventArgs e)
+        {
+            dgDnsEntries.Rows.Clear();
+        }
+
+        private void btnFlushDNS_Click(object sender, EventArgs e)
+        {
+            // Start the child process.
+            Process p = new Process();
+            // Redirect the output stream of the child process.
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = "ipconfig";
+            p.StartInfo.Arguments = "/flushdns";
+            p.Start();
+            // Do not wait for the child process to exit before
+            // reading to the end of its redirected stream.
+            // p.WaitForExit();
+            // Read the output stream first and then wait.
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            MessageBox.Show(output, "Flush DNS results");
+        }
+
+        private void btnResolveAll_Click(object sender, EventArgs e)
+        {
+            foreach (var hostname in GetCurrentHostnameList())
+            {
+                EnqueueResolve(hostname);
+            }
+        }
+
+        private void btnPasteFromClipboard_Click(object sender, EventArgs e)
+        {
+            var entries = Clipboard.GetText();
+
+            var hostnames = entries.Split(new string[] { ",", "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            DataGridViewRow row;
+
+            foreach (var hostname in hostnames)
+            {
+                row = new DataGridViewRow();
+                row.CreateCells(dgDnsEntries, hostname, "");
+                dgDnsEntries.Rows.Add(row);
+            }
+
+            foreach (var hostname in hostnames.Distinct())
+            {
+                EnqueueResolve(hostname);
+            }
+        }
+
+        #endregion User Events
+
+        #region Private Methods
+
+        private void EnqueueResolve(string hostname)
+        {
+            lock (dnsRequestsToDo)
+            {
+                dnsRequestsToDo.Enqueue(hostname);
+                UpdateIPAddress(hostname, "pending...");
+            }
+        }
+
+        private void UpdateIPAddress(string hostname, string newValue)
+        {
+            this.InvokeAction(@this =>
+                {
+                    for (int i = 0; i < dgDnsEntries.Rows.Count; i++)
+                    {
+                        if (hostname.Equals(((string)dgDnsEntries.Rows[i].Cells[0].Value), StringComparison.CurrentCultureIgnoreCase))
+                        {
+                            dgDnsEntries.Rows[i].Cells[1].Value = newValue;
+                        }
+                    }
+                });
+        }
+
+        private IEnumerable<string> GetCurrentHostnameList()
+        {
+            var hostnames = new List<string>();
+
+            for (int i = 0; i < dgDnsEntries.Rows.Count; i++)
+            {
+                var hostname = (string)dgDnsEntries.Rows[i].Cells[0].Value;
+
+                if (String.IsNullOrEmpty(hostname) == false)
+                {
+                    hostnames.Add(hostname);
+                }
+            }
+
+            return hostnames.Distinct();
+        }
+
+        #endregion Private Methods
+
+        #region Events
+
+        private void tmrQueuePoll_Tick(object sender, EventArgs e)
+        {
+            tmrQueuePoll.Enabled = false;
+
+            try
+            {
+                lock (dnsRequestsToDo)
+                {
+                    while (dnsRequestsToDo.Any())
+                    {
+                        var hostname = dnsRequestsToDo.Dequeue();
+
+                        try
+                        {
+                            var ip = Dns.GetHostEntry(hostname);
+                            UpdateIPAddress(hostname, ip.AddressList[0].ToString());
+                        }
+                        catch (Exception ex)
+                        {
+                            UpdateIPAddress(hostname, ex.Message);
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                tmrQueuePoll.Enabled = true;
+            }
+        }
+
+        #endregion Events
+    }
+}
